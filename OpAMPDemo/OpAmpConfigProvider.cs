0c8ab2de79bf2fc65f52cba5eb06d1c0ff096038
Matthew Hensley <matthew.hensley@grafana.com>
2024-03-05 19:21:26 -0500

    use protobuf to load config

--- OpAMPDemo/OpAmpConfigProvider.cs
+++ OpAMPDemo/OpAmpConfigProvider.cs
@@ -1,30 +1,38 @@
-﻿using YamlDotNet.Serialization;
+﻿using opamp.proto;
+using System.Net.Http.Headers;
+using System.Text;
+using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
 namespace OpAMPDemo
 {
     public class OpAmpConfigSource : IConfigurationSource
     {
+        public string ServiceInstanceId { get; set; }
         public string ServiceName { get; set; }
 
         public IConfigurationProvider Build(IConfigurationBuilder builder)
         {
-            return new OpAmpConfigProvider(ServiceName);
+            return new OpAmpConfigProvider(ServiceInstanceId, ServiceName);
         }
     }
 
     public class OpAmpConfigProvider : ConfigurationProvider, IDisposable
     {
+        public static string AGENT_URL = "http://localhost:12345";
+
+        private readonly string _serviceInstanceId;
         private readonly string _serviceName;
         private readonly HttpClient _httpClient;
         private readonly Timer _timer;
         private readonly IDeserializer _deserializer;
 
-        public OpAmpConfigProvider(string serviceName)
+        public OpAmpConfigProvider(string serviceInstanceId, string serviceName)
         {
+            _serviceInstanceId = serviceInstanceId;
             _serviceName = serviceName;
             _httpClient = new HttpClient();
-            _timer = new Timer(_ => LoadInternal(), null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+            _timer = new Timer(_ => LoadInternalOpAmp(), null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
             _deserializer = new DeserializerBuilder()
                 .WithNamingConvention(NullNamingConvention.Instance)
                 .Build();
@@ -34,14 +42,40 @@ namespace OpAMPDemo
         {
             // dummy load method, timer immediately calls LoadInternal asynchronously
         }
-
-        internal async void LoadInternal()
+        public void Dispose()
         {
-            var url = $"http://localhost:12345/api/v0/debugdial/{_serviceName}";
+            _timer.Dispose();
+            _httpClient.Dispose();
+        }
+
+        internal async void LoadInternalHttp()
+        {
+            var url = $"{AGENT_URL}/api/v0/debugdial/{_serviceName}";
             var resp = await _httpClient.GetAsync(url);
             var res = await resp.Content.ReadAsStringAsync();
             var config = _deserializer.Deserialize<OpAmpConfig>(res);
+            applyConfig(config);
+        }
 
+        internal async void LoadInternalOpAmp()
+        {
+            var agentToServer = getAgentToServer(_serviceInstanceId, _serviceName);
+            var stream = new MemoryStream();
+            ProtoBuf.Serializer.Serialize(stream, agentToServer);
+
+            var url = $"{AGENT_URL}/api/v0/debugdial";
+            var content = new ByteArrayContent(stream.ToArray());
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-protobuf");
+            var res = await _httpClient.PostAsync(url, content);
+
+            var bytes = await res.Content.ReadAsStreamAsync();
+            var serverToAgent = ProtoBuf.Serializer.Deserialize<ServerToAgent>(bytes);
+            var config = extractRemoteConfig(serverToAgent, _serviceName);
+            applyConfig(config);
+        }
+
+        internal void applyConfig(OpAmpConfig config)
+        {
             foreach (var logLevel in config.logLevels)
             {
                 var level = logLevel.level switch
@@ -65,10 +99,45 @@ namespace OpAMPDemo
             OnReload();
         }
 
-        public void Dispose()
+        private opamp.proto.AgentToServer getAgentToServer(string serviceInstanceId, string serviceName)
         {
-            _timer.Dispose();
-            _httpClient.Dispose();
+            return new opamp.proto.AgentToServer
+            {
+                InstanceUid = serviceInstanceId,
+                AgentDescription = new opamp.proto.AgentDescription
+                {
+                    IdentifyingAttributes =
+                    {
+                        new opamp.proto.KeyValue
+                        {
+                            Key = "service.name",
+                            Value = new opamp.proto.AnyValue
+                            {
+                                StringValue = serviceName
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        private OpAmpConfig extractRemoteConfig(opamp.proto.ServerToAgent msg, string serviceName)
+        {
+            if (msg.RemoteConfig.Config.ConfigMaps.ContainsKey(serviceName) == false)
+            {
+                return new OpAmpConfig();
+            }
+
+            var agentConfigFile = msg.RemoteConfig.Config.ConfigMaps[serviceName];
+
+            if (agentConfigFile == null)
+            {
+                return new OpAmpConfig();
+            }
+
+            var s = Encoding.UTF8.GetString(agentConfigFile.Body, 0, agentConfigFile.Body.Length);
+            var config = _deserializer.Deserialize<OpAmpConfig>(s);
+            return config;
         }
     }
 }
